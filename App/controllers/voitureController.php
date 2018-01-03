b88285788cfcd0dc62f3a30bbac9eaadaa15d8d1
hssounaomar <hsouna.omar@gmail.com>
2018-01-03 18:40:02 +0100

    add model voirure test

--- App/controllers/voitureController.php
+++ App/controllers/voitureController.php
@@ -6,7 +6,7 @@
  * Time: 08:56
  */
 
-class voitureController
+class voitureController extends Controller
 {
     public function home($name =''){
         $this->view('template/header');
---
+++ App/models/Voiture.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 31/12/2017
+ * Time: 09:17
+ */
+
+class Voiture
+{
+    private $id;
+    private $nom;
+    private $prix;
+    private $note;
+    private $description;
+    private $marque;
+    private $longitude;
+    private $latitude;
+    
+}
